b297d20dda3e10d46ad6a614f6511658f67b15cf
Thierry Suzanne <thierry.suzanne78@orange.fr>
2022-04-08 12:10:32 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,8 @@
+import burger from './modules/Burger'
+import reveal from './modules/Reveal'
+import scrollSpy from './modules/ScrollSpy'
+
+//hideNavbar("header", "header")
+scrollSpy()
+burger('header', 'burger','header-nav-menu-item', null, 'main')
+reveal()
